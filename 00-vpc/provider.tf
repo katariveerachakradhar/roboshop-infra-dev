f0b587a8e227b201858e0c8ae8722ac36564035f
Veera <veeravamshi1@gmail.com>
2025-06-30 16:40:01 +0530

    infra

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-nanau"
+    bucket = "84s-remote-state-dev"
     key    = "roboshop-dev-vpc"
     region = "us-east-1"
     encrypt        = true
