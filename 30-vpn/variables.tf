15643d16223894969c6d9a83beaac97661053bdf
Veera <veeravamshi1@gmail.com>
2025-07-04 10:51:11 +0530

    roboshop-infra-dev

--- 30-vpn/variables.tf
+++ 30-vpn/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z03167101DYK3BKNE90QI"
+}
+
+variable "zone_name" {
+    default = "veerakatari.site"
+}
