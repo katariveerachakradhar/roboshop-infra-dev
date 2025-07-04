4e8d2a31faa433f07e7fa77cd87d02dc5220cbfb
Veera <veeravamshi1@gmail.com>
2025-07-04 10:49:56 +0530

    roboshop-infra-dev

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03167101DYK3BKNE90QI"
+}
+
+variable "zone_name" {
+    default = "veerakatari.site"
+}
