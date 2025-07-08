42edbd0e5129754ff572e139aba9f9a20a111ba3
Veera <veeravamshi1@gmail.com>
2025-07-08 10:11:30 +0530

    roboshop-infra-dev

--- 90-components/provider.tf
+++ 90-components/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "84s-remote-state"
+    bucket = "84s-remote"
     key    = "roboshop-dev-components"
     region = "us-east-1"
     encrypt        = true
